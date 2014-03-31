b243fc3392cd0acf372592548cfd9a6aab07dbb3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-31 13:25:54 -0400

    Specifying high level features of ROF

--- spec/lib/rof_spec.rb
+++ spec/lib/rof_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe ROF do
+  context '.validate' do
 
+  end
+
+  context '.create' do
+
+  end
+
+  context '.update' do
+
+  end
+
+  context '.patch' do
+
+  end
 end
