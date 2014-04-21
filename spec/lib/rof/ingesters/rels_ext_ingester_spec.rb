d0784cc44bea7c41d6cbfe9ad07c2cfcb6d1083c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-04-21 16:35:39 -0700

    Updating RelsExtIngester to test fedora behavior

--- spec/lib/rof/ingesters/rels_ext_ingester_spec.rb
+++ spec/lib/rof/ingesters/rels_ext_ingester_spec.rb
@@ -7,9 +7,26 @@ module ROF
       let(:item) {
         { "pid" => '1234', "rels-ext" => { "isPartOf" => ["vecnet:d217qs82g"] } }
       }
-      subject { described_class.new(models: models, item: item, ) }
+      let(:fedora_document) { nil }
+      let(:expected_content) { "<rdf:RDF xmlns:ns0=\"info:fedora/fedora-system:def/model#\" xmlns:ns1=\"info:fedora/fedora-system:def/relations-external#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"><rdf:Description rdf:about=\"info:fedora/1234\"><ns0:hasModel rdf:resource=\"info:fedora/afmodel:Shoe\"/><ns1:isPartOf rdf:resource=\"vecnet:d217qs82g\"/></rdf:Description></rdf:RDF>" }
 
-      its(:call) { should eq "<rdf:RDF xmlns:ns0=\"info:fedora/fedora-system:def/model#\" xmlns:ns1=\"info:fedora/fedora-system:def/relations-external#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"><rdf:Description rdf:about=\"info:fedora/1234\"><ns0:hasModel rdf:resource=\"info:fedora/afmodel:Shoe\"/><ns1:isPartOf rdf:resource=\"vecnet:d217qs82g\"/></rdf:Description></rdf:RDF>" }
+      subject { described_class.new(models: models, item: item, fedora_document: fedora_document) }
+
+      context 'without a fedora document' do
+        its(:call) { should eq expected_content }
+      end
+
+      context 'with a fedora document' do
+        let(:fedora_document) { double }
+        let(:rels_ext) { double }
+        it 'should save the document' do
+          fedora_document.should_receive(:[]).with('RELS-EXT').and_return(rels_ext)
+          rels_ext.should_receive(:content=).with(expected_content)
+          rels_ext.should_receive(:mimeType=).with("application/rdf+xml")
+          rels_ext.should_receive(:save)
+          subject.call
+        end
+      end
     end
   end
 end
