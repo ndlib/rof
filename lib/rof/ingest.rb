0df51ce697832e87fc44582a1c03d8a24c6f742a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-05 16:51:04 -0400

    Extracting functionality of datastream ingester
    These are tested by a singular test, but that will need to change

--- lib/rof/ingest.rb
+++ lib/rof/ingest.rb
@@ -71,44 +71,6 @@ module ROF
 
   def self.ingest_datastream(dsname, item, fdoc, search_paths)
     Ingesters::DatastreamIngester.call(dsname: dsname, item: item, fedora_document: fdoc, search_paths: search_paths)
-    # What kind of content is there?
-    ds_content = item[dsname]
-    ds_filename = item["#{dsname}-file"]
-    if ds_filename && ds_content
-      raise SourceError.new("Both #{dsname} and #{dsname}-file are present.")
-    end
-
-    md = {"mime-type" => "text/plain",
-          "label" => "",
-          "versionable" => true,
-          "control-group" => "M",
-    }
-    if item["#{dsname}-meta"]
-      md.merge!(item["#{dsname}-meta"])
-    end
-
-    # NOTE(dbrower): this could be refactored a bit. I was trying to keep the
-    # same path for whether fdoc is nil or not as much as possible.
-    ds = nil
-    if fdoc
-      ds = fdoc[dsname]
-      # TODO(dbrower): maybe verify these options to be within bounds?
-      ds.controlGroup = md["control-group"]
-      ds.dsLabel = md["label"]
-      ds.versionable = md["versionable"]
-      ds.mimeType = md["mime-type"]
-    end
-    need_close = false
-    if ds_filename
-      ds_content = self.find_file_and_open(ds_filename, search_paths, "r")
-      need_close = true
-    end
-    if ds
-      ds.content = ds_content if ds_content
-      ds.save
-    end
-  ensure
-    ds_content.close if ds_content && need_close
   end
 
   def self.ingest_rights_metadata(item, fdoc)
--- lib/rof/ingesters/datastream_ingester.rb
+++ lib/rof/ingesters/datastream_ingester.rb
@@ -14,7 +14,44 @@ module ROF
       end
 
       def call
-      end
+        # What kind of content is there?
+        ds_content = item[dsname]
+        ds_filename = item["#{dsname}-file"]
+        if ds_filename && ds_content
+          raise SourceError.new("Both #{dsname} and #{dsname}-file are present.")
+        end
+
+        md = {"mime-type" => "text/plain",
+              "label" => "",
+              "versionable" => true,
+              "control-group" => "M",
+              }
+        if item["#{dsname}-meta"]
+          md.merge!(item["#{dsname}-meta"])
+        end
+
+        # NOTE(dbrower): this could be refactored a bit. I was trying to keep the
+        # same path for whether fdoc is nil or not as much as possible.
+        ds = nil
+        if fdoc
+          ds = fdoc[dsname]
+          # TODO(dbrower): maybe verify these options to be within bounds?
+          ds.controlGroup = md["control-group"]
+          ds.dsLabel = md["label"]
+          ds.versionable = md["versionable"]
+          ds.mimeType = md["mime-type"]
+        end
+        need_close = false
+        if ds_filename
+          ds_content = self.find_file_and_open(ds_filename, search_paths, "r")
+          need_close = true
+        end
+        if ds
+          ds.content = ds_content if ds_content
+          ds.save
+        end
+      ensure
+      ds_content.close if ds_content && need_close      end
     end
   end
 end
