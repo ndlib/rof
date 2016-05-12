09b12519d28c9a6663ea5c64518ccc49665f656b
Don Brower <dbrower@nd.edu>
2016-05-12 15:31:53 -0400

    Remove tabs

--- lib/rof/filters/bendo.rb
+++ lib/rof/filters/bendo.rb
@@ -4,7 +4,7 @@ module ROF
   module Filters
 
     # If bendo server is set , add it into datasreams that contain an URl referencing bendo
-	  #
+    #
     class Bendo
       def initialize(bendo=nil)
         @bendo = bendo
@@ -12,22 +12,22 @@ module ROF
 
       def process(obj_list)
 
-      ends_meta = Regexp.new('(.+)-meta')
+        ends_meta = Regexp.new('(.+)-meta')
 
-      # for *-meta objects containing "URL", sub in bendo string if provided
+        # for *-meta objects containing "URL", sub in bendo string if provided
 
-      obj_list.map! do |obj|
-	      obj.map do |name, value|
-		      if name =~ ends_meta
-			      if obj[name]["URL"] && @bendo
-				      obj[name]["URL"] = obj[name]["URL"].sub("bendo:",@bendo)
-			      end
-		      end
-	      end
-	      # print object 
-	      obj
+        obj_list.map! do |obj|
+          obj.map do |name, value|
+            if name =~ ends_meta
+              if obj[name]["URL"] && @bendo
+                obj[name]["URL"] = obj[name]["URL"].sub("bendo:",@bendo)
+              end
+            end
+          end
+          # print object
+          obj
+        end
       end
-      end
-      end
-     end
     end
+  end
+end
