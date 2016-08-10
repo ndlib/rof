52a146ff46291d8b96c3ff095abf7d7eb0a6c4df
msuhovec <suhovecky@nd.edu>
2016-08-10 13:23:20 -0400

    code inspection changes
    #31

--- lib/rof/compare_rof.rb
+++ lib/rof/compare_rof.rb
@@ -12,10 +12,10 @@ module ROF
    def self.fedora_vs_bendo( fedora_rof, bendo_rof, output)
 
      error_count = 0
-     error_count += compare_rights( fedora_rof, bendo_rof, output)
-     error_count += compare_rels_ext(fedora_rof, bendo_rof, output)
-     error_count += compare_metadata(fedora_rof, bendo_rof, output)
-     error_count += compare_everything_else(fedora_rof, bendo_rof, output)
+     error_count += compare_rights( fedora_rof[0], bendo_rof[0], output)
+     error_count += compare_rels_ext(fedora_rof[0], bendo_rof[0], output)
+     error_count += compare_metadata(fedora_rof[0], bendo_rof[0], output)
+     error_count += compare_everything_else(fedora_rof[0], bendo_rof[0], output)
      error_count
    end
 
@@ -26,9 +26,10 @@ module ROF
      
       # Use same comparison scheme on all rights
       [ 'read' , 'read-groups', 'edit', 'edit-groups', 'edit-users', 'embargo-date'].each { |attribute|
-        exist_count = rights_exist(attribute, fedora_rof[0], bendo_rof[0])
+        exist_count = rights_exist(attribute, fedora_rof, bendo_rof)
 	return 1 if exist_count == 1
-	error_count += rights_equal(attribute, fedora_rof[0], bendo_rof[0]) if exist_count == 2
+	error_count += rights_equal(attribute, fedora_rof, bendo_rof) if exist_count == 2
+	break if error_count != 0
       }
 
       error_count
@@ -59,9 +60,10 @@ module ROF
     # convert RELS-EXT sections to RDF::graph and compater w/ rdf-isomorphic
     def self.compare_rels_ext( fedora, bendo, output)
       error_count =0
-      bendo[0]['rels-ext']['@context'] = ROF::RelsExtRefContext.dup
-      bendo_rdf = RDF::Graph.new << JSON::LD::API.toRdf(bendo[0]['rels-ext'])
-      fedora_rdf = RDF::Graph.new << JSON::LD::API.toRdf(fedora[0]['rels-ext'])
+      bendo['rels-ext']['@context'] = ROF::RelsExtRefContext.dup if ! bendo['rels-ext'].has_key?('@context')
+      fedora['rels-ext']['@context'] = ROF::RelsExtRefContext.dup if ! fedora['rels-ext'].has_key?('@context')
+      bendo_rdf = RDF::Graph.new << JSON::LD::API.toRdf(bendo['rels-ext'])
+      fedora_rdf = RDF::Graph.new << JSON::LD::API.toRdf(fedora['rels-ext'])
       error_count +=1 if ! bendo_rdf.isomorphic_with? fedora_rdf
       error_count
     end
@@ -69,49 +71,29 @@ module ROF
     # convert metadata sections to RDF::graph and compater w/ rdf-isomorphic
     def self.compare_metadata( fedora, bendo, output)
       error_count =0
-      bendo[0]['metadata']['@context'] = ROF::RdfContext.dup
-      bendo_rdf = RDF::Graph.new << JSON::LD::API.toRdf(bendo[0]['metadata'])
-      fedora_rdf = RDF::Graph.new << JSON::LD::API.toRdf(fedora[0]['metadata'])
+      bendo['metadata']['@context'] = ROF::RdfContext.dup
+      bendo_rdf = RDF::Graph.new << JSON::LD::API.toRdf(bendo['metadata'])
+      fedora_rdf = RDF::Graph.new << JSON::LD::API.toRdf(fedora['metadata'])
       error_count +=1 if ! bendo_rdf.isomorphic_with? fedora_rdf
       error_count
     end
 
-    #
+    # compare what remains
     def self.compare_everything_else( fedora, bendo, output)
       error_count =0
       fedora = remove_others(fedora)
       bendo = remove_others(bendo)
-      bendo = sort_recurse( bendo )
-      fedora = sort_recurse( fedora )
-      error_count +=1 if fedora.to_s != bendo.to_s
+      error_count = 1 if bendo != fedora
       error_count
     end
 
-    # recursive sort through hashes and arrays
-    def self.sort_recurse( item )
-      if item.is_a?(Hash)
-	item.each do | key, value |
-          item[ key] = sort_recurse( item[ key] )
-	end
-        return Hash[item.sort]
-      elsif item.is_a?(Array)
-	item.each do | idx, value |
-          item[ idx] = sort_recurse( item[ idx] )
-	end
-	return item.sort
-      else
-        return item
-      end
-    end
-
-    #
-    def self.remove_others( list)
-      list[0].delete('rights')
-      list[0].delete('rels-ext')
-      list[0].delete('metadata')
-      list[0].delete('thumbnail-file')
-      sl = Hash[list[0].sort]
-      sl
+    # remove elements we've dealt with already
+    def self.remove_others( rof_object)
+      rof_object.delete('rights')
+      rof_object.delete('rels-ext')
+      rof_object.delete('metadata')
+      rof_object.delete('thumbnail-file')
+      rof_object
     end
   end
 end
