45493eb5ebdd96bb3dcab6c4c083686329abab54
msuhovec <suhovecky@nd.edu>
2016-11-28 16:24:46 -0500

    DLTP-710
    issue-57

---
+++ spec/lib/rof/get_from_fedora_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe ROF::FedoraToRof do
+  it 'handles embargo presence or absence' do
+
+    rights_tests = [
+      ['<embargo> <human/> <machine> <date>2017-08-01</date> </machine> </embargo>', true],
+      ['<embargo> <human/> <machine> <date></date> </machine> </embargo>', false],
+      ['<embargo> <human/> <machine/> </embargo>', false]
+    ]
+
+    begin
+
+      rights_tests.each do |this_test|
+        xml_doc = REXML::Document.new(this_test[0])
+        root = xml_doc.root
+        rights = ROF::FedoraToRof.testEmbargoDate(root)
+        expect(rights).to eq(this_test[1])
+      end
+    end
+  end
+end
