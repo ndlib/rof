8ddce61c0a824a25a0e975a3399bd0603609b7db
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-01-31 09:30:15 -0500

    Refactoring OsfToRof for instance ttl_data
    Instead of passing the ttl_data as parameters, set an instance
    variable. I found it a bit confusing that throughout the code,
    references to `ttl_data` could be either the full ttl_data (i.e. an
    array) or an element of the original `ttl_data` (i.e. a Hash).
    
    This is my attempt to clean that up and provide greater clarity.

--- lib/rof/osf_to_rof.rb
+++ lib/rof/osf_to_rof.rb
@@ -24,14 +24,19 @@ module ROF
     def call
       rof_array = []
       return {} if project.nil?
-      ttl_data = ttl_from_targz(project_identifier + '.ttl')
-      rof_array[0] = build_archive_record(ttl_data)
+      @ttl_data = ttl_from_targz(project_identifier + '.ttl')
+      rof_array[0] = build_archive_record
       rof_array
     end
 
     private
 
     attr_reader :config, :project
+
+    # this is an array- the addition elements are the contributor(s)
+    # @return [Array<Hash>]
+    # @see #ttl_from_targz
+    attr_reader :ttl_data
 
     def project_identifier
       project.fetch('project_identifier')
@@ -45,65 +50,63 @@ module ROF
 
     # extracts given ttl file from JHU tar.gz package
     # - assumed to live under data/obj/root
+    # @return [Array<Hash>] the first element is the "work" and the additional elements, if any, are the contributor(s)
     def ttl_from_targz(ttl_filename)
       package_dir = config.fetch('package_dir')
       ttl_path = File.join(project_identifier, 'data/obj/root', ttl_filename)
       ROF::Utility.file_from_targz(File.join(package_dir, project_identifier + '.tar.gz'), ttl_path)
-      ttl_data = fetch_from_ttl(File.join(package_dir, ttl_path))
-      # this is an array- the addition elements are the contributor(s)
-      ttl_data
+      fetch_from_ttl(File.join(package_dir, ttl_path))
     end
 
     # Maps RELS-EXT
-    def map_rels_ext(_ttl_data)
+    def map_rels_ext
       rels_ext = {}
       rels_ext['@context'] = ROF::RelsExtRefContext.dup
       rels_ext
     end
 
     # sets metadata
-    def map_metadata(ttl_data)
+    def map_metadata
       metadata = {}
       metadata['@context'] = ROF::RdfContext.dup
       # metdata derived from project ttl file
       metadata['dc:created'] = Time.iso8601(ttl_data[0][@osf_map['dc:created']][0]['@value']).to_date.iso8601 + 'Z'
       metadata['dc:title'] = ttl_data[0][@osf_map['dc:title']][0]['@value']
-      metadata['dc:description'] =
-        ttl_data[0][@osf_map['dc:description']][0]['@value']
-      metadata['dc:subject'] = map_subject(ttl_data[0])
+      metadata['dc:description'] = ttl_data[0][@osf_map['dc:description']][0]['@value']
+      metadata['dc:subject'] = map_subject
       # metadata derived from osf_projects data, passed from UI
       metadata['dc:source'] = 'https://osf.io/' + project_identifier
       metadata['dc:creator#adminstrative_unit'] = project['administrative_unit']
       metadata['dc:creator#affiliation'] = project['affiliation']
-      metadata['dc:creator'] = map_creator(ttl_data)
+      metadata['dc:creator'] = map_creator
       metadata
     end
 
     # Constructs OsfArchive Record from ttl_data, data from the UI form,
     # and task config data
-    def build_archive_record(ttl_data)
+    def build_archive_record
       this_rof = {}
       this_rof['owner'] = project['owner']
       this_rof['type'] = 'OsfArchive'
-      this_rof['rights'] = map_rights(ttl_data[0])
-      this_rof['rels-ext'] = map_rels_ext(ttl_data[0])
-      this_rof['metadata'] = map_metadata(ttl_data)
+      this_rof['rights'] = map_rights
+      this_rof['rels-ext'] = map_rels_ext
+      this_rof['metadata'] = map_metadata
       this_rof['files'] = [project_identifier + '.tar.gz']
       this_rof
     end
 
     # sets subject
-    def map_subject(ttl_data)
-      if ttl_data.key?(@osf_map['dc:subject'])
-        return ttl_data[@osf_map['dc:subject']][0]['@value']
+    def map_subject
+      if ttl_data[0].key?(@osf_map['dc:subject'])
+        return ttl_data[0][@osf_map['dc:subject']][0]['@value']
       end
       ''
     end
 
     # figures out the rights
-    def map_rights(ttl_data)
+    def map_rights
       rights = {}
-      if ttl_data[@osf_map['isPublic']][0]['@value'] == 'true'
+      if ttl_data[0][@osf_map['isPublic']][0]['@value'] == 'true'
         rights['read-groups'] = ['public']
       end
       rights
@@ -111,9 +114,10 @@ module ROF
 
     # sets the creator- needs to read another ttl for the User data
     # only contrubutors with isBibliographic true are considered
-    def map_creator(ttl_data)
+    def map_creator
       creator = []
       ttl_data[0][@osf_map['hasContributor']].each do |contributor|
+        # Looping through the primary document and the contributors
         ttl_data.each do |item|
           next unless item['@id'] == contributor['@id']
           if item[@osf_map['isBibliographic']][0]['@value'] == 'true'
