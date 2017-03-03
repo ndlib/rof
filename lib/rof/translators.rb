42bf92225b01a5e584da928a843585092e056cb3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-03 10:00:28 -0500

    Adding a ROF::Translators module
    In an effort to organize the junk drawer, I'm trying to name things and
    find their place.

---
+++ lib/rof/translators.rb
@@ -0,0 +1,9 @@
+require 'rof/translators/csv_to_rof'
+module ROF
+  # A namespace for organizing translating classes. A translating class is responsible for
+  # converting from one format to another format (e.g. CSV to ROF).
+  #
+  # @see ROF::Translators::CsvToRof
+  module Translators
+  end
+end
