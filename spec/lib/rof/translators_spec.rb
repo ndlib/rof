7d73ff71ebc339617f9bb02dff79d039849d1711
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-03 10:37:09 -0500

    Adding delegation test to ensure wiring up

--- spec/lib/rof/translators_spec.rb
+++ spec/lib/rof/translators_spec.rb
@@ -9,5 +9,16 @@ module ROF
         described_class.csv_to_rof(contents)
       end
     end
+
+    describe '.fedora_to_rof' do
+      it 'delegates to FedoraToRof.run' do
+        contents = double
+        fedora = double
+        outfile = double
+        config = double
+        expect(described_class::FedoraToRof).to receive(:run).with(contents, fedora, outfile, config)
+        described_class.fedora_to_rof(contents, fedora, outfile, config)
+      end
+    end
   end
 end
