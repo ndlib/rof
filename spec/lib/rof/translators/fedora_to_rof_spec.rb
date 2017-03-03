8ba65939ea2e0cc89048263405de8e7008941d97
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-03 11:06:24 -0500

    Extracting utility method for has_embargo_date?

--- spec/lib/rof/translators/fedora_to_rof_spec.rb
+++ spec/lib/rof/translators/fedora_to_rof_spec.rb
@@ -1,21 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe ROF::Translators::FedoraToRof do
-  it 'handles embargo presence or absence' do
-    rights_tests = [
-      ['<embargo> <human/> <machine> <date>2017-08-01</date> </machine> </embargo>', true],
-      ['<embargo> <human/> <machine> <date></date> </machine> </embargo>', false],
-      ['<embargo> <human/> <machine/> </embargo>', false]
-    ]
-
-    rights_tests.each do |this_test|
-      xml_doc = REXML::Document.new(this_test[0])
-      root = xml_doc.root
-      rights = described_class.has_embargo_date(root)
-      expect(rights).to eq(this_test[1])
-    end
-  end
-
   it "retrieves a fedora object and converts it to ROF" do
     expected_output = [{
       "pid" => "und:dev0012829m",
