1d55b04d56e37ebfa7af4d60770a2a495bc9ba88
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-03 10:59:41 -0500

    Refactoring to test outer looping function

--- spec/lib/rof/translators/fedora_to_rof_spec.rb
+++ spec/lib/rof/translators/fedora_to_rof_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe ROF::Translators::FedoraToRof do
   end
 
   it "retrieves a fedora object and converts it to ROF" do
-    expected_output = {
+    expected_output = [{
       "pid" => "und:dev0012829m",
       "type" => "fobject",
       "af-model" => "GenericFile",
@@ -30,18 +30,19 @@ RSpec.describe ROF::Translators::FedoraToRof do
         "read-groups" => ["registered"],
         "edit" => ["dbrower"]
       },
-      "properties" => "<fields>\n<depositor>batch_ingest</depositor>\n<owner>dbrower</owner>\n</fields>\n",
       "properties-meta" => {"mime-type" => "text/xml"},
+      "properties" => "<fields>\n<depositor>batch_ingest</depositor>\n<owner>dbrower</owner>\n</fields>\n",
+      "content-meta" => {"label"=>"bonnie+chauncey", "mime-type"=>"application/octet-stream", "URL"=>"http://libvirt9.library.nd.edu:14000/item/dev0012826k/bonnie+chauncey"},
       "metadata" => {
-	 "@context"=> {
+        "@context"=> {
            "bibo"=>"http://purl.org/ontology/bibo/",
            "dc"=>"http://purl.org/dc/terms/",
-           "ms" => 'http://www.ndltd.org/standards/metadata/etdms/1.1/',
            "ebucore"=>"http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#",
            "foaf"=>"http://xmlns.com/foaf/0.1/",
            'hydramata-rel' => 'http://projecthydra.org/ns/relations#',
            "mrel"=>"http://id.loc.gov/vocabulary/relators/",
-	   "nd"=>"https://library.nd.edu/ns/terms/",
+           "ms" => 'http://www.ndltd.org/standards/metadata/etdms/1.1/',
+           "nd"=>"https://library.nd.edu/ns/terms/",
            "rdfs"=>"http://www.w3.org/2000/01/rdf-schema#",
            'ths' => 'http://id.loc.gov/vocabulary/relators/',
            "vracore"=>"http://purl.org/vra/",
@@ -57,9 +58,8 @@ RSpec.describe ROF::Translators::FedoraToRof do
       },
       "bendo-item" => "dev0012826k",
       "characterization-meta" => {"mime-type"=>"text/xml"},
-      "content-meta" => {"label"=>"bonnie+chauncey", "mime-type"=>"application/octet-stream", "URL"=>"http://libvirt9.library.nd.edu:14000/item/dev0012826k/bonnie+chauncey"},
       "thumbnail-meta" => {"label"=>"File Datastream", "mime-type"=>"image/png"},
-    }
+    }]
     pid = 'und:dev0012829m'
     config = {}
     fedora = {}
@@ -67,8 +67,11 @@ RSpec.describe ROF::Translators::FedoraToRof do
     fedora[:user] = 'fedoraAdmin'
     fedora[:password] = 'fedoraAdmin'
     VCR.use_cassette("fedora_to_rof1") do
-      fedora_data =  ROF::Translators::FedoraToRof.GetFromFedora(pid, fedora, config)
-      expect(fedora_data).to eq(expected_output)
+      outfile = double(close: true, write: true)
+      described_class.run([pid], fedora, outfile, config)
+      expect(outfile).to have_received(:write).with(JSON.pretty_generate(expected_output))
+      # fedora_data =  ROF::Translators::FedoraToRof.GetFromFedora(pid, fedora, config)
+      # expect(fedora_data).to eq(expected_output)
     end
   end
 end
